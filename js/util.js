1f92e65c283f48d677f3acfe1a64e3f442762598
Alex Chew <alex_chew@msn.com>
2019-01-06 18:31:07 +0800

    change to https

--- js/util.js
+++ js/util.js
@@ -9,7 +9,7 @@ app.globalData={
         userInfo:null,
     };
 app.config={
-    auth_api:"http://www.shouxinjk.net/ilife-wechat",//获取UserInfo后端服务
+    auth_api:"https://data.shouxinjk.net/ilife-wechat",//获取UserInfo后端服务
     data_api:"https://data.shouxinjk.net/_db/sea",//数据存取服务
     search_api:"https://data.pcitech.cn",//搜索服务:内容搜索后缀为 /stuff/_search
     message_api:"https://data.shouxinjk.net/kafka-rest"//日志等消息服务（kafka）
