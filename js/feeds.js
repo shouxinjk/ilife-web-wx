59b2c29b1e3de3e5fffc214d0d5aec24fd12404e
Alex Chew <alex_chew@msn.com>
2019-01-12 11:19:40 +0800

    add feed module

--- js/feeds.js
+++ js/feeds.js
@@ -1,11 +1,22 @@
 // 文档加载完毕后执行
 $(document).ready(function ()
 {
+        //根据屏幕大小计算字体大小
+    const oHtml = document.getElementsByTagName('html')[0]
+    const width = oHtml.clientWidth;
+    var rootFontSize = 12 * (width / 1440);// 在1440px的屏幕基准像素为12px
+    rootFontSize = rootFontSize <8 ? 8:rootFontSize;//最小为8px
+    rootFontSize = rootFontSize >16 ? 16:rootFontSize;//最大为18px
+    oHtml.style.fontSize = rootFontSize+ "px";   
     //显示加载状态
     showloading(true);
     //处理参数
     var args = getQuery();//获取参数
     //category = args["category"]?args["category"]:0; //如果是跳转，需要获取当前目录
+    $('#waterfall').NewWaterfall({
+        width: width-20,
+        delay: 100,
+    });
 
     //设置浏览用户
     if(app.globalData.userInfo){
@@ -14,11 +25,6 @@ $(document).ready(function ()
     }
     loadPersons();//加载用户
     loadData();//加载数据：默认使用当前用户查询
-
-    var mySwiper = new Swiper ('.swiper-container', {
-      direction: 'horizental',
-      loop: false
-    })
 
 });
 
@@ -34,10 +40,11 @@ var page = {//翻页控制
 };
 
 var persons = [];
-var currentPerson = null;
+var currentPerson = "oQhcg5fVusZ47QQLH2Vu7TWIAUHs";
 
 setInterval(function ()
 {
+    //console.log("interval",$(window).scrollTop(),$(document).height(),$(window).height(),$(document).height() - $(window).height() - dist);
     if ($(window).scrollTop() >= $(document).height() - $(window).height() - dist && !loading)
     {
         // 表示开始加载
@@ -45,8 +52,10 @@ setInterval(function ()
 
         // 加载内容
         if(items.length < num){//如果内容未获取到本地则继续获取
+            //console.log("load from remote ");
             loadData();
         }else{//否则使用本地内容填充
+            //console.log("load from locale ");
             insertItem();
         }
     }
@@ -84,46 +93,58 @@ function loadData() {
       "Content-Type": "application/json",
       "Authorization": "Basic ZWxhc3RpYzpjaGFuZ2VtZQ=="
     };
-    util.AJAX("https://data.pcitech.cn/actions/_search", function (res) {
-      console.log("Feed::loadData now parsing search result.", res);
-      if (res.data.hits.hits.length == 0) {//如果没有更多，则显示提示文字
-          shownomore(true);
-          showloading(false);
-      } else {
-        var arr = res.data.hits.hits;// 获取结果
-        for (var i = 0; i < arr.length; i++) {//将搜索结果放入列表
-          var record = arr[i]._source;
-          items.push(record.item);
+
+    $.ajax({
+        url:"https://data.pcitech.cn/actions/_search",
+        type:"post",
+        data:JSON.stringify(esQuery),//注意：nginx启用CORS配置后不能直接通过JSON对象传值
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":"Basic ZWxhc3RpYzpjaGFuZ2VtZQ=="
+        },
+        crossDomain: true,
+        success:function(data){
+            console.log("Feed::loadData success.",data);
+            if(data.hits.hits.length==0){//如果没有内容，则显示提示文字
+                shownomore(true);
+                showloading(false);
+            }else{
+                //更新总页数
+                var total = data.hits.total;
+                page.total = (total + page.size - 1) / page.size;
+                //更新当前翻页
+                page.current = page.current + 1;
+                //装载具体条目
+                var hits = data.hits.hits;
+                for(var i = 0 ; i < hits.length ; i++){
+                    items.push(hits[i]._source.item);
+                }
+                //insertItem();
+                showloading(false);
+            }
         }
-        insertItem();//显示到页面
-
-        //更新翻页信息
-        var total = res.data.hits.total;//获取匹配总数
-        page.total = (total + page.size - 1) / page.size;
-        page.current = page.current + 1;
-
-        // 更新状态
-        shownomore(false);
-        showloading(false);
-      }
-    }, "post", JSON.stringify(esQuery), esHeader);
+    });
   }
 
 //load related persons
 function loadPersons() {
-    util.AJAX(app.config.data_api+"user/users", function (res) {
-      var arr = res.data;
+    util.AJAX(app.config.data_api+"/user/users", function (res) {
+      var arr = res;
       //从列表内过滤掉当前用户：当前用户永远排在第一个
       for (var i = 0; i < arr.length; i++) {
         var u = arr[i];
-        if (u._key != app.globalData.userInfo._key){
+        if (app.globalData.userInfo == null || u._key != app.globalData.userInfo._key){
           persons.push(u);
         }
       }
       //将用户显示到页面
-       for (var i = 0; i < persons.length; i++) {
+      for (var i = 0; i < persons.length; i++) {
         insertPerson(persons[i]);
-      }           
+      }    
+      //显示滑动条
+        var mySwiper = new Swiper ('.swiper-container', {
+          slidesPerView: 7,
+        });             
     });
 }
 
@@ -136,19 +157,25 @@ function loadPersons() {
 */
 function insertPerson(person){
     // 显示HTML
+    var html = '';
+    html += '<div class="swiper-slide">';
+    html += '<div class="person" id="person'+person._key+'">';
     var style= person._key==currentPerson?'-selected':'';
-    var image = '<img class="person-img'+style+'" src="'+person.avatarUrl+'"/>';
-    var title = '<div class="person-name">'+person.nickName+'</div>';
-    $("#persons").append("<div class="swiper-slide person" id='person"+person._key+"'>" + image +title+ "</div>");
+    html += '<img class="person-img'+style+'" src="'+person.avatarUrl+'"/>';
+    html += '<div class="person-name">'+person.nickName+'</div>';
+    html += '</div>';
+    html += '</div>';
+    $("#persons").append(html);
 
     //注册事件:点击后切换用户
-    $("person"+person._key+"']").click(function(){
+    $("person"+person._key).click(function(){
         changePerson(person._key);
     });
 }
 
 //显示没有更多内容
 function shownomore(flag){
+    $("#nomore").empty();
     var html = '';
     if(flag){
         html += '<text> 没有更多了 </text>';
@@ -158,75 +185,22 @@ function shownomore(flag){
 
 //显示正在加载提示
 function showloading(flag){
+    $("#loading").empty();
+    loading = flag;
     var html = '';
     if(flag){
-        html += '<image src="http://www.uusucai.com/d/file/web/tubiao/2015/06/19/5fc1dd5e77261ac65f5412ba66b466e4.gif" mode="widthFix"/>';
+        html += '<image src="http://www.uusucai.com/d/file/web/tubiao/2015/06/19/5fc1dd5e77261ac65f5412ba66b466e4.gif"/>';
     }
     $("#loading").append(html);
 }
 
-/*
-<view class="WxMasonry">
-    <div  id="{{item._key}}" url="../detail/detail?id={{item._key}}">
-        <view class="mainbody">
-          <!--image-->
-          <image class="WxMasonryImage" id="{{item.id}}" mode="aspectFill" src="{{item.images[0]}}" />
-          <!--author-->
-          <!--view class="host">
-            <view class="host-avatar">
-              <image class="host-avatar-img" mode="aspectFill" bindload="WxMasonryImageLoad" src="{{item.images[0]}}"/>
-            </view>
-            <text class="host-name">{{item.title}}</text>
-          </view-->
-        </view>
-        <!--来源及匹配度-->
-        <view class="shopping">
-          <view class="shopping-summary">
-            <!--view>
-              <image class="shopping-icon" mode="aspectFit" src="http://www.shouxinjk.net/list/images/flags/{{item.distributor.country}}.png" wx:if="{{item.distributor.country.length>0}}"/>
-            </view-->
-              <image class="shopping-icon" class="shopping-icon" mode="aspectFit" src="http://www.shouxinjk.net/list/images/source/{{item.source}}.png" wx:if="{{item.source.length>0}}"/>
-              <text class="box-title" wx:if="{{item.distributor.name.length>0}}">{{item.distributor.name}}</text>
-              <!--text class="title" wx:if="{{item.seller.name.length>0}}">{{item.seller.name}}</text-->
-          </view>
-          <view class="likes">
-            <!--text class="likes-number">{{countTotal}}</text-->
-            <text class="currency" wx:if="{{item.price.currency}}">{{item.price.currency}}</text>
-            <text class="currency" wx:if="{{item.price.currency==null}}">¥</text>
-            <text class="price-sale" wx:if="{{item.price.sale}}">{{item.price.sale}}</text>
-            <text class="price-bid" wx:if="{{item.price.bid}}">/</text>
-            <text class="price-bid" wx:if="{{item.price.bid}}">{{item.price.bid}}</text>                                
-          </view>      
-        </view> 
-        <view class="list-box">
-          <!--标题与标签-->
-          <view class="main-item">
-            <!--title-->
-            <view class="list-box-title">{{item.title}}</view>
-            <!--tags-->
-            <view class="tags" wx:if="{{item.tags.length>0}}">
-              <navigator wx:for="{{item.tags}}" id="{{tag}}" wx:for-item="tag" wx:key="">
-                  <view class="tag-text" wx:if="{{tag.length>0}}">#{{tag}}</view>
-              </navigator>
-            </view>
-          </view>
-          <!--相关指数-->
-          <!--view class="similarity">
-            <view class="similarity-title">相关度%</view>
-            <view class="similarity-score">87.5</view>
-          </view-->
-        </view>
-    </div>
-</view>
-*/
-
 //将item显示到页面
 function insertItem(){
     var item = items[num-1];//从本地取一条item
-
+    console.log("Feed::insertItem add item to html.",num,item);
     var html = '';
     html += '<li><div class="WxMasonry">';
-    html += '<div  id="item'+item._key+'">';
+    html += '<div id="item'+item._key+'">';
     html += htmlItemImage(item);
     html += htmlItemSummary(item);
     html += htmlItemTags(item);
@@ -235,19 +209,19 @@ function insertItem(){
     $("#waterfall").append(html);
 
     //注册事件
-    $("#item'"+item._key+"']").click(function(){
+    $("#item"+item._key).click(function(){
         //跳转到详情页
         window.location.href = "detail.html?id="+item._key;
     });
     // 表示加载结束
+    showloading(false);
     num++;
-    loading = false;
 }
 
 function htmlItemImage(item){
     var html = '';
     html += '<div class="mainbody">';
-    html += '<img class="WxMasonryImage" id="'+item._key+'" mode="aspectFill" src="'+item.images[0]+'" />';
+    html += '<img class="WxMasonryImage" id="'+item._key+'" src="'+item.images[0]+'" width="100%" height="200px" />';
     html += '</div>';
     return html;
 }
@@ -257,7 +231,7 @@ function htmlItemSummary(item){
     html += '<div class="shopping">';
     html += '<div class="shopping-summary">';
     if(item.source.length>0){
-        html += '<img class="shopping-icon" class="shopping-icon" mode="aspectFit" src="http://www.shouxinjk.net/list/images/source/'+item.source+'.png"/>';
+        html += '<img class="shopping-icon" class="shopping-icon" src="http://www.shouxinjk.net/list/images/source/'+item.source+'.png"/>';
     }
     if(item.distributor.name.length>0){
         html += '<text class="box-title">'+item.distributor.name+'</text>';
@@ -283,7 +257,7 @@ function htmlItemTags(item){
     var html = '';
     html += '<div class="list-box">';
     html += '<div class="main-item">';
-    html += '<div class="list-box-title">{{item.title}}</div>';
+    html += '<div class="list-box-title">'+item.title+'</div>';
     if(item.tags.length>0){
         html += '<div class="tags">';
         for(var i=0;i<item.tags.length;i++){
