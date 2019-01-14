0f7c95a891e60fe5a32298d5157c0adc244f385c
Alex Chew <alex_chew@msn.com>
2019-01-14 21:04:11 +0800

    bug fix

--- js/info2.js
+++ js/info2.js
@@ -228,7 +228,7 @@ function registerShareHandler(){
                 wx.onMenuShareTimeline({
                     title:stuff?stuff.title:"小确幸，大生活", // 分享标题
                     link:window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl:stuff?stuff.images[0]:"http://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(12)+".jpeg", // 分享图标
+                    imgUrl:stuff?stuff.images[0]:"http://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(11)+".jpeg", // 分享图标
                     success: function () {
                         // 用户点击了分享后执行的回调函数
                         logstash(stuff,"mp","share timeline",function(){
@@ -241,7 +241,7 @@ function registerShareHandler(){
                     title:stuff?stuff.title:"小确幸，大生活", // 分享标题
                     desc:stuff?stuff.tags:"Live is all about having a good time.", // 分享标题
                     link:window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                    imgUrl: stuff?stuff.images[0]:"http://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(12)+".jpeg", // 分享图标
+                    imgUrl: stuff?stuff.images[0]:"http://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(11)+".jpeg", // 分享图标
                     type: '', // 分享类型,music、video或link，不填默认为link
                     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
                     success: function () {
