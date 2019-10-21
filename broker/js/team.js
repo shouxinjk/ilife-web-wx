467361c4e1abed6ceed01618896ba89303151233
Alex Chew <alex_chew@msn.com>
2019-10-21 19:40:26 +0800

    bug fix

--- broker/js/team.js
+++ broker/js/team.js
@@ -98,7 +98,7 @@ function updateBroker(broker) {
     console.log("try to update broker.[broker]",broker);
     util.AJAX(app.config.sx_api+"/mod/broker/rest/"+broker.id, function (res) {
         console.log("update broker successfully.",res);
-    },"PATCH");
+    },"PATCH",broker,{'content-type': 'application/json'});
 }
 
 //根据openid查询加载broker
