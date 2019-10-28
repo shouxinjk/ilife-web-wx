06c830d18cb0d28a519e95df7cd5ee9eef6a1c5a
Alex Chew <alex_chew@msn.com>
2019-10-28 12:04:26 +0800

    add user register page

---
+++ js/user-register.js
@@ -0,0 +1,226 @@
+// 文档加载完毕后执行
+$(document).ready(function ()
+{
+    //根据屏幕大小计算字体大小
+    const oHtml = document.getElementsByTagName('html')[0]
+    const width = oHtml.clientWidth;
+    var rootFontSize = 12 * (width / 1440);// 在1440px的屏幕基准像素为12px
+    rootFontSize = rootFontSize <8 ? 8:rootFontSize;//最小为8px
+    rootFontSize = rootFontSize >16 ? 16:rootFontSize;//最大为18px
+    oHtml.style.fontSize = rootFontSize+ "px";   
+
+    //计算图片流宽度：根据屏幕宽度计算，最小显示2列
+    if(width < 2*columnWidth){//如果屏幕不能并排2列，则调整图片宽度
+        //columnWidth = (width-columnMargin*4)/2;//由于每一个图片左右均留白，故2列有4个留白
+    }
+
+    //显示加载状态
+    //showloading(true);
+    //处理参数
+    var args = getQuery();//获取参数
+    if(args["id"]){
+        currentPerson = args["id"]; //如果传入参数则使用传入值
+    }
+
+    //获取推荐者openid
+    if(args["toUserOpenId"]){
+        toUserOpenId = args["toUserOpenId"]; //获取推荐者Openid
+        loadToUserByOpenid(toUserOpenId);//加载推荐者
+        loadConnection();//加载关联关系
+    }else{
+        console.log("Error. Cannot get toUserOpenId.");
+    }  
+
+    $("body").css("background-color","#fff");//更改body背景为白色
+
+    loadPerson(currentPerson);//加载用户
+
+    //注册事件：切换操作类型
+    $(".order-cell").click(function(e){
+        changeActionType(e);
+    });
+
+    //注册事件：点击提交按钮
+    $("#submitBtn").click(function(){
+        updateConnection();
+    });
+
+});
+
+util.getUserInfo();//从本地加载cookie
+
+var columnWidth = 800;//默认宽度600px
+var columnMargin = 5;//默认留白5px
+var loading = false;
+var dist = 500;
+var num = 1;//需要加载的内容下标
+
+var items = [];//所有画像列表
+var personaConns = [];//已经选中的列表
+var selectedPersonaIds = [];//已经选中的列表,仅存储ID，便于比对使用
+
+var page = {
+    size:20,//每页条数
+    total:1,//总页数
+    current:-1//当前翻页
+};
+
+var currentActionType = '';//当前操作类型
+var tagging = '';//操作对应的action 如buy view like 等
+
+var currentPerson = app.globalData.userInfo?app.globalData.userInfo._key:null;
+var userInfo=app.globalData.userInfo;//默认为当前用户
+
+var toUserOpenId="";
+
+var connJson = {
+    name:"关心我的TA"
+};
+
+//加载当前修改的connection
+function loadConnection(){
+    var query={
+            collection: "connections", 
+            example: { 
+                _from:"user_users/"+userInfo._key,//发起端为当前用户
+                _to:"user_users/"+toUserOpenId//目的端为推荐者
+            },
+            limit:1
+        };   
+    var header={
+        "Content-Type":"application/json",
+        Authorization:"Basic aWxpZmU6aWxpZmU="
+    }; 
+    util.AJAX(app.config.data_api+"/_api/simple/by-example", function (res) {
+        //showloading(false);
+        console.log("User::Settings::loadConnection try to retrive user connections.", res);
+        if(res && res.count==0){//如果没有则表示有问题哦
+            console.log("something wrong. we cannot get user-user connections.");
+        }else{//否则更新关系名称
+            var hits = res.result;
+            connJson = hits[0];
+            $("#connName").val(connJson.name);//设置默认关系名称
+        }
+    }, "PUT",query,header);
+}
+
+function updateConnection(){
+    var newName = $("#connName").val().trim().length==0?connJson.name:$("#connName").val();
+    var data={
+            name:newName
+        };   
+    var header={
+        "Content-Type":"application/json",
+        Authorization:"Basic aWxpZmU6aWxpZmU="
+    }; 
+    util.AJAX(app.config.data_api+"/_api/document/connections/"+connJson._key, function (res) {
+        //showloading(false);
+        console.log("User::Settings::updateConnection update user connections done.", res);
+        window.location.href="index.html";//跳转到首页
+    }, "PATCH",data,header);
+}
+
+//load toUser 加载推荐者
+function loadToUserByOpenid(personId) {
+    console.log("try to load toUser person info.",personId);
+    util.AJAX(app.config.data_api+"/user/users/"+personId, function (res) {
+        console.log("load toUser person info.",personId,res);
+        $("#fromUser").val(res.nickName?res.nickName:res.nickname);//更新推荐者名称
+    });
+}
+
+//load person
+function loadPerson(personId) {
+    console.log("try to load person info.",personId);
+    util.AJAX(app.config.data_api+"/user/users/"+personId, function (res) {
+        console.log("load person info.",personId,res);
+        userInfo = res;
+        currentPerson = res._key;
+        insertPerson(userInfo);//TODO:当前直接显示默认信息，需要改进为显示broker信息，包括等级、个性化logo等
+        //loadData();
+        loadBrokerByOpenid(res._key);//根据openid加载broker信息
+    });
+}
+
+//更新Broker
+function updateBroker(broker) {
+    console.log("try to update broker.[broker]",broker);
+    util.AJAX(app.config.sx_api+"/mod/broker/rest/"+broker.id, function (res) {
+        console.log("update broker successfully.",res);
+    },"PUT",broker,{ "Content-Type":"application/json" });
+}
+
+//根据openid查询加载broker
+function loadBrokerByOpenid(openid) {
+    console.log("try to load broker info by openid.[openid]",openid);
+    util.AJAX(app.config.sx_api+"/mod/broker/rest/brokerByOpenid/"+openid, function (res) {
+        console.log("load broker info.",openid,res);
+        if (res.status) {
+            insertBroker(res.data);//显示达人信息
+        }
+    });
+}
+
+function insertPerson(person){
+    // 显示HTML
+    var html = '';
+    html += '<div class="info-general">';
+    html += '<img class="general-icon" src="'+person.avatarUrl+'" height="60px"/>';
+    html += '</div>';
+    html += '<div class="info-detail">';
+    html += '<div class="info-text info-blank">'+person.nickName+'</div>';
+    html += '<div class="info-text info-blank" id="brokerHint">'+(person.province?person.province:"")+(person.city?(" "+person.city):"")+'</div>';
+    html += '<div class="info-text info-blank" id="brokerLink"></div>';
+    html += '</div>';
+    $("#user").append(html);
+}
+
+function insertBroker(broker){
+    $("#brokerLink").html('<a href="broker/task.html">进入达人后台</a>');
+    $("#brokerHint").html("达人级别："+broker.level);
+}
+
+//显示没有更多内容
+function shownomore(flag){
+  if(flag){
+    $("#footer").toggleClass("footer-hide",false);
+    $("#footer").toggleClass("footer-show",true);
+  }else{
+    $("#footer").toggleClass("footer-hide",true);
+    $("#footer").toggleClass("footer-show",false);
+  }
+}
+
+//显示正在加载提示
+function showloading(flag){
+  if(flag){
+    $("#loading").toggleClass("loading-hide",false);
+    $("#loading").toggleClass("loading-show",true);
+  }else{
+    $("#loading").toggleClass("loading-hide",true);
+    $("#loading").toggleClass("loading-show",false);    
+  }
+}
+
+function changeActionType (e) {
+    console.log("now try to change action type.",e);
+    //首先清除原来高亮状态
+    if(currentActionType.trim().length>0){
+        $("#"+currentActionType+" img").attr("src","images/"+currentActionType+".png"); 
+        $("#"+currentActionType+" div").removeClass("actiontype-selected");
+        $("#"+currentActionType+" div").addClass("actiontype");  
+    }  
+    //更改并高亮显示
+    currentActionType = e.currentTarget.id;
+    tagging = e.currentTarget.dataset.tagging;
+    if (app.globalData.isDebug) console.log("User::ChangeActionType change action type.",currentActionType,tagging);
+    if(currentActionType.trim().length>0){
+        $("#"+currentActionType+" img").attr("src","images/"+currentActionType+"-selected.png"); 
+        $("#"+currentActionType+" div").removeClass("actiontype");
+        $("#"+currentActionType+" div").addClass("actiontype-selected");  
+    } 
+
+    //跳转到相应页面
+    window.location.href = currentActionType+".html";
+}
+
