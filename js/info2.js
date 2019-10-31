6c347220b1984ad81f190385360631e9304deace
Alex Chew <alex_chew@msn.com>
2019-10-31 15:08:21 +0800

    add wx share

--- js/info2.js
+++ js/info2.js
@@ -367,9 +367,9 @@ function registerShareHandler(){
     $.ajax({
         url:app.config.auth_api+"/wechat/jssdk/ticket",
         type:"get",
-        //data:{url:window.location.href},
-        data:{url:shareUrl},
+        data:{url:window.location.href},//重要：微信分享的URL必须和浏览器浏览地址保持一致！！
         success:function(json){
+            console.log("===got jssdk ticket===\n",json);
             wx.config({
                 debug:false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: json.appId, // 必填，公众号的唯一标识
@@ -377,8 +377,17 @@ function registerShareHandler(){
                 nonceStr: json.nonceStr, // 必填，生成签名的随机串
                 signature: json.signature,// 必填，签名
                 jsApiList: [
-                    'onMenuShareTimeline', 'onMenuShareAppMessage',
-                    'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone'
+                   // 'onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone',
+                  'updateAppMessageShareData',
+                  'updateTimelineShareData',
+                  'onMenuShareAppMessage',
+                  'onMenuShareTimeline',
+                  'chooseWXPay',
+                  'showOptionMenu',
+                  "hideMenuItems",
+                  "showMenuItems",
+                  "onMenuShareTimeline",
+                  'onMenuShareAppMessage'                   
                 ] // 必填，需要使用的JS接口列表
             });
             wx.ready(function() {
