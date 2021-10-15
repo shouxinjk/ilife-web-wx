8f558a4a8a45c04061e3c645def64d6c78f75c0e
Alex Chew <alex_chew@msn.com>
2021-10-15 09:16:24 +0800

    add tracing codes to jump page

--- js/go.js
+++ js/go.js
@@ -10,8 +10,18 @@ $(document).ready(function ()
     }
     var args = getQuery();//获取参数
     var id = args["id"]?args["id"]:0;
+
+    from = args["from"]?args["from"]:"mp";//可能为groupmessage,timeline等
+    fromUser = args["fromUser"]?args["fromUser"]:"";//从连接中获取分享用户ID
+    fromBroker = args["fromBroker"]?args["fromBroker"]:"";//从连接中获取分享达人ID。重要：将依据此进行收益计算
+
     jump(id);
 });
+
+//记录分享用户、分享达人
+var from = "mp";//链接来源，默认为公众号进入
+var fromUser = "";
+var fromBroker = "";
 
 function jump(id){//获取详细内容
     $.ajax({
@@ -19,7 +29,7 @@ function jump(id){//获取详细内容
         type:"get",
         data:{},
         success:function(item){
-            logstash(item,client,"buy step2",function(){
+            logstash(item,from,"buy step2",shareUserId,shareBrokerId,function(){
                 if(client == "wap"){
                     window.location.href=item.link.wap2?item.link.wap2:item.link.wap;
                 }else{
