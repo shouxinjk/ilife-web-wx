90876d98ae4c0c3c8905d7f038ad65ef428c7962
Alex Chew <alex_chew@msn.com>
2022-03-29 22:00:02 +0800

    add publisher module

---
+++ publisher/js/articles.js
@@ -0,0 +1,417 @@
+// 文档加载完毕后执行
+$(document).ready(function ()
+{
+    //根据屏幕大小计算字体大小
+    const oHtml = document.getElementsByTagName('html')[0]
+    const width = oHtml.clientWidth;
+    var rootFontSize = 12 * (width / 1440);// 在1440px的屏幕基准像素为12px
+    rootFontSize = rootFontSize <8 ? 8:rootFontSize;//最小为8px
+    rootFontSize = rootFontSize >16 ? 16:rootFontSize;//最大为18px
+    oHtml.style.fontSize = rootFontSize+ "px";   
+
+    //计算图片流宽度：根据屏幕宽度计算，最小显示2列
+    if(width < 2*columnWidth){//如果屏幕不能并排2列，则调整图片宽度
+        //columnWidth = (width-columnMargin*4)/2;//由于每一个图片左右均留白，故2列有4个留白
+    }
+    var args = getQuery();//获取参数
+    $('#waterfall').NewWaterfall({
+        width: columnWidth,
+        delay: 100,
+    });
+
+    //显示加载状态
+    showloading(true);
+    //处理参数
+    var args = getQuery();//获取参数
+    if(args["id"]){
+        currentPerson = args["id"]; //如果传入参数则使用传入值
+    }
+    if(args["filter"]){
+        filter = args["filter"]; //如果传入参数则使用传入值：all、byBroker
+    }
+
+    $("body").css("background-color","#fff");//更改body背景为白色
+
+    //加载达人信息
+    loadBrokerInfo();
+
+    loadPerson(currentPerson);//加载用户
+
+    //注册事件：切换操作类型
+    $(".order-cell").click(function(e){
+        changeActionType(e);
+    });
+
+    //注册点击创建按钮事件 ：显示新建页面，然后跳转到index.html
+    $("#createArticleBtn").click(function(e){
+        window.location.href="articles-add.html"
+    });
+    
+    //检查是否有缓存事件
+    resultCheck();
+
+});
+
+util.getUserInfo();//从本地加载cookie
+
+//设置默认logo
+var logo = "https://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(23)+".jpeg";
+
+var columnWidth = 800;//默认宽度600px
+var columnMargin = 5;//默认留白5px
+var loading = false;
+var dist = 500;
+var num = 1;//需要加载的内容下标
+
+var filter = "all";//my、all。数据查询规则：默认为查询全部
+
+var items = [];//所有画像列表
+var page = {
+    size:10,//每页条数
+    total:1,//总页数
+    current:-1//当前翻页
+};
+
+var currentActionType = '';//当前操作类型
+var tagging = '';//操作对应的action 如buy view like 等
+
+var currentPerson = app.globalData.userInfo?app.globalData.userInfo._key:null;
+var userInfo=app.globalData.userInfo;//默认为当前用户
+
+var currentBroker = null;
+var broker = {};//当前达人
+
+var sxTimer = null;
+var sxStartTimestamp=new Date().getTime();//定时器如果超过2分
+var sxLoopCount = 1000;//定时器运行100次即停止，即30秒
+
+//优先从cookie加载达人信息
+function loadBrokerInfo(){
+  broker = util.getBrokerInfo();
+  currentBroker = broker.id;
+}
+
+function registerTimer(brokerId){
+    currentBroker = brokerId;
+    sxTimer = setInterval(function ()
+    {
+        console.log("Timer Boards::registerTimer start load boards Timer.");
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - dist && !loading)
+        {
+            console.log("Boards::registerTimer start load boards.");
+            // 表示开始加载
+            loading = true;
+            showloading(true);
+
+            // 加载内容
+            if(items.length < num){//如果内容未获取到本地则继续获取
+                //读取待阅读列表
+                loadItems();
+                //有用户操作则恢复计数器
+                console.log("reset loop count.");
+                sxLoopCount = 100;                
+            }else{//否则使用本地内容填充
+                insertItem();
+            }
+        }
+
+        //计数器自减，到时即停止
+        if(--sxLoopCount<0){
+            unregisterTimer();
+        }
+    }, 300);
+}
+
+function unregisterTimer(){
+    console.log("clear timer.");
+    clearInterval(sxTimer);
+}
+
+
+/*
+加载待阅读文章列表：
+按照阅豆高低倒序排列得到最新文章列表
+*/
+function loadItems(){
+    util.AJAX(app.config.sx_api+"/wx/wxArticle/rest/pending-articles", function (res) {
+        showloading(false);
+        console.log("Publisher::Articles::loadItems try to retrive pending articles.", res)
+        if(res && res.length==0){//如果没有画像则提示，
+            shownomore();
+        }else{//否则显示到页面
+            //更新当前翻页
+            page.current = page.current + 1;
+            //装载具体条目
+            var hits = res;
+            for(var i = 0 ; i < hits.length ; i++){
+                items.push(hits[i]);
+            }
+            insertItem();
+        }
+    }, "GET",{from:(page.current+1)*page.size,to:(page.current+1)*page.size+page.size},{});
+}
+
+//将item显示到页面
+function insertItem(){
+    // 加载内容
+    var item = items[num-1];
+    if(!item){
+        shownomore(true);
+        return;
+    }
+
+    //文章无logo，随机指定一个。设置为发布者LOGO，或者直接忽略
+    logo = "https://www.biglistoflittlethings.com/list/images/logo"+getRandomInt(23)+".jpeg";
+    if(item.logo)
+        logo = item.logo;
+    //判断有无置顶广告位
+    var tags = "";
+    if(item.advertise){//如果有广告位则显示置顶
+        tags += "<span style='margin:2px auto;padding:2px;border:1px solid red;color:red;border-radius:16px;font-size:12px;line-height:20px;'>置顶</span>";
+    }
+    tags += "<span style='margin-right:5px;padding:0 2px;border:1px solid red;color:red;border-radius:5px;font-size:12px;line-height:16px;'>置顶</span>";
+    var advertise = "<img src='https://www.biglistoflittlethings.com/ilife-web-wx/images/rocket.png' width='16' height='16'/>&nbsp;";
+
+    var title = "<div class='title'>"+tags+item.title+advertise+"</div>";
+    var image = "<img src='"+logo+"' style='width:60px;object-fit:cover;'/>";
+    var description = "<div class='description'>"+item.updateDate+"</div>";
+
+    var btns = "<div class='btns'><div id='article-"+item.id+"' data-id='"+item.id+"'>前往批阅</div></div>";
+
+    $("#waterfall").append("<li><div class='task' data='"+item.id+"' data-title='"+item.title+"' data-url='"+item.url+"'><div class='task-logo'>" + image +"</div><div class='task-tags'>" +title +description+"</div></li>");
+    num++;
+
+    //注册事件
+    $("div[data='"+item.id+"']").click(function(){
+        //cookie缓存记录当前浏览文章，返回时检查
+        console.log("Publisher::Articles now jump to article.");
+        var expDate = new Date();
+        expDate.setTime(expDate.getTime() + (60 * 1000)); // 60秒钟后自动失效：避免用户直接叉掉页面不再回来    
+        var readingArticle = {
+            id:$(this).attr("data"),//文章id
+            title:$(this).attr("data-title"),//文章标题
+            url:$(this).attr("data-url"),//文章URL
+            startTime: new Date().getTime()//开始时间戳：需要超过10秒
+        };
+               
+        console.log("Publisher::Articles save article to cookie.",readingArticle);
+        $.cookie('sxArticle', JSON.stringify(readingArticle), { expires: expDate, path: '/' });  //把浏览中的文章id写入cookie便于记录阅读数       
+
+        //跳转到原始页面完成阅读
+        console.log("Publisher::Articles now jump to article.");
+        //window.location.href = "../index.html";   
+        window.location.href = $(this).attr("data-url");          
+
+    });
+
+    // 表示加载结束
+    loading = false;
+}
+
+//load person
+function loadPerson(personId) {
+    console.log("try to load person info.",personId);
+    util.AJAX(app.config.data_api+"/user/users/"+personId, function (res) {
+        console.log("load person info.",personId,res);
+        userInfo = res;
+        currentPerson = res._key;
+        insertPerson(userInfo);//TODO:当前直接显示默认信息，需要改进为显示broker信息，包括等级、个性化logo等
+        //loadData();
+        loadBrokerByOpenid(res._key);//根据openid加载broker信息
+    });
+}
+
+//更新Broker
+function updateBroker(broker) {
+    console.log("try to update broker.[broker]",broker);
+    util.AJAX(app.config.sx_api+"/mod/broker/rest/"+broker.id, function (res) {
+        console.log("update broker successfully.",res);
+    },"PUT",broker,{ "Content-Type":"application/json" });
+}
+
+//根据openid查询加载broker
+function loadBrokerByOpenid(openid) {
+    console.log("try to load broker info by openid.[openid]",openid);
+    util.AJAX(app.config.sx_api+"/mod/broker/rest/brokerByOpenid/"+openid, function (res) {
+        console.log("load broker info.",openid,res);
+        if (res.status) {
+            $.cookie('sxBroker', JSON.stringify(res.data), {  path: '/' });     
+            broker = res.data; 
+            insertBroker(res.data);//显示达人信息
+            registerTimer(res.data.id);//加载该达人的board列表
+        }
+    });
+}
+
+function insertPerson(person){
+    // 显示HTML
+    var html = '';
+    html += '<div class="info-general">';
+    html += '<img class="general-icon" src="'+person.avatarUrl+'" height="60px"/>';
+    html += '</div>';
+    html += '<div class="info-detail">';
+    html += '<div class="info-text info-blank">'+person.nickName+'</div>';
+    html += '<div class="info-text info-blank" id="brokerHint">'+(person.province?person.province:"")+(person.city?(" "+person.city):"")+'</div>';
+    html += '<div class="info-text info-blank" id="brokerLink"><a href="../user.html">返回用户后台</a></div>';
+    html += '</div>';
+    $("#user").append(html);
+}
+
+function insertBroker(broker){
+    $("#brokerHint").html("流量主");
+}
+
+//显示没有更多内容
+function shownomore(flag){
+  if(flag){
+    unregisterTimer();
+    $("#footer").toggleClass("footer-hide",false);
+    $("#footer").toggleClass("footer-show",true);
+  }else{
+    $("#footer").toggleClass("footer-hide",true);
+    $("#footer").toggleClass("footer-show",false);
+  }
+}
+
+//显示正在加载提示
+function showloading(flag){
+  if(flag){
+    $("#loading").toggleClass("loading-hide",false);
+    $("#loading").toggleClass("loading-show",true);
+  }else{
+    $("#loading").toggleClass("loading-hide",true);
+    $("#loading").toggleClass("loading-show",false);    
+  }
+}
+
+function changeActionType (e) {
+    console.log("now try to change action type.",e);
+    //首先清除原来高亮状态
+    if(currentActionType.trim().length>0){
+        $("#"+currentActionType+" img").attr("src","images/"+currentActionType+".png"); 
+        $("#"+currentActionType+" div").removeClass("actiontype-selected");
+        $("#"+currentActionType+" div").addClass("actiontype");  
+    }  
+    //更改并高亮显示
+    currentActionType = e.currentTarget.id;
+    tagging = e.currentTarget.dataset.tagging;
+    if (app.globalData.isDebug) console.log("User::ChangeActionType change action type.",currentActionType,tagging);
+    if(currentActionType.trim().length>0){
+        $("#"+currentActionType+" img").attr("src","images/"+currentActionType+"-selected.png"); 
+        $("#"+currentActionType+" div").removeClass("actiontype");
+        $("#"+currentActionType+" div").addClass("actiontype-selected");  
+    } 
+
+    //跳转到相应页面
+    window.location.href = currentActionType+".html";
+}
+
+//检查阅读效果：如果检查到有cookie，则显示阅读数确认框
+function resultCheck(){
+    var articleInfo = $.cookie('sxArticle');
+    console.log("load articleInfo from cookie.",articleInfo);
+    if(articleInfo && articleInfo.trim().length>0){
+        console.log("get articleInfo info from cookie.",articleInfo);
+        var article = JSON.parse(articleInfo);
+        //检查时间是否已达到10秒
+        var duration = new Date().getTime() - Number(article.startTime);
+        if( duration > 10000){
+            //显示数据填报表单
+            $.blockUI({ message: $('#checkform'),
+                css:{ 
+                    padding:        10, 
+                    margin:         0, 
+                    width:          '60%', 
+                    top:            '40%', 
+                    left:           '20%', 
+                    textAlign:      'center', 
+                    color:          '#000', 
+                    border:         '1px solid silver', 
+                    backgroundColor:'#fff', 
+                    cursor:         'normal' 
+                },
+                overlayCSS:  { 
+                    backgroundColor: '#000', 
+                    opacity:         0.7, 
+                    cursor:          'normal' 
+                }
+            }); 
+            $("#btnNo").click(function(){
+                $.cookie('sxArticle', "", { path: '/' }); //清除cookie重新来过
+                $.unblockUI(); 
+            });
+            $("#btnYes").click(function(){//完成阅读后的奖励操作
+                //检查数字：必填。TODO：此处需要判断是否胡乱填报
+                var readCount = Number($("#viewNumbers").val());
+                if(readCount <=0 ){
+                    $("#viewNumbers").css("border","1px solid red");
+                }else{
+                    console.log("try to submit read event.");
+                    $.cookie('sxArticle', "", { path: '/' }); //清除cookie重新来过
+                    costPoints(article);
+                }
+            });
+        }else{//提示超过10秒才可以
+            console.log("不到10秒，不能奖励",duration/1000);
+            siiimpleToast.message('亲，要超过10秒才能奖励阅豆哦~~',{
+                  position: 'bottom|center'
+                });
+            //清除cookie重新来过
+            $.cookie('sxArticle', "", { path: '/' });  
+        }
+    }else{
+      console.log("no article from cookie.",articleInfo);
+    }
+}
+
+//完成阅读扣除及奖励
+function costPoints(article){
+    //先扣除阅豆
+    console.log("try to commit read event.",userInfo._key);
+    $.ajax({
+        url:app.config.sx_api+"/wx/wxArticle/rest/exposure/"+article.id+"/"+userInfo._key,
+        type:"post",
+        success:function(res){
+            console.log("cost point succeed.",res);
+            logPointCostEvent(article,res);//记录本次阅读历史
+            //解除屏幕锁屏
+            $.unblockUI(); 
+        }
+    })      
+}
+
+//记录文章阅读历史：publisher内包含有文章发布者信息：openid,brokerId,nickname,avatarUrl 消耗的点数points
+//需要补全：阅读者openid，nickname，avatarUrl，阅读时间，阅读报数，文章ID
+//记录ID为：文章ID+阅读者openid md5
+function logPointCostEvent(article,publisher){
+    var readCount = Number($("#viewNumbers").val());
+    $("#viewNumbers").css("border","1px solid silver");//恢复标准风格
+    $("#viewNumbers").val("");//清空原有数值，避免交叉
+    $.ajax({
+        url:app.config.analyze_api+"?query=insert into ilife.reads values ('"+hex_md5(article.id+userInfo._key)+"','"+
+            publisher.openid+"','"+
+            publisher.brokerId+"','"+
+            publisher.nickname+"','"+
+            publisher.avatarUrl+"','"+
+            userInfo._key+"','"+
+            userInfo.nickname+"','"+
+            userInfo.avatarUrl+"','"+
+            article.id+"','"+
+            article.title+"','"+
+            article.url+"',"+
+            publisher.points+","+readCount+",now())",
+        type:"post",
+        //data:{},
+        headers:{
+            "Authorization":"Basic ZGVmYXVsdDohQG1AbjA1"
+        },         
+        success:function(json){
+            console.log("===reads inserted===\n",json);
+            //从当前列表中删除该文章
+            $("div[data='"+article.id+"']").remove();
+        }
+    });     
+}
+
+
+
