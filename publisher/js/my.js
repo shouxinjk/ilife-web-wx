fe5c703da5ce75b1916c6cf2a7b5fab5d0120a7c
Alex Chew <alex_chew@msn.com>
2022-04-11 21:55:56 +0800

    bug fix

--- publisher/js/my.js
+++ publisher/js/my.js
@@ -1083,7 +1083,7 @@ function payOrder(payInfo){
               
                 wx.chooseWXPay({
                     appId: payInfo.appId,
-                  timeStamp: payInfo.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                  timeStamp: payInfo.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                   nonceStr: payInfo.nonceStr, // 支付签名随机串，不长于 32 位
                   package: 'prepay_id='+payInfo.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
                   signType: 'MD5', // 微信支付V3的传入RSA,微信支付V2的传入格式与V2统一下单的签名格式保持一致
