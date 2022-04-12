c7791cfd0191dc8b45b94f4161a66ddfd5a921f8
Alex Chew <alex_chew@msn.com>
2022-04-12 09:29:04 +0800

    bug fix

--- publisher/js/my.js
+++ publisher/js/my.js
@@ -1052,23 +1052,13 @@ function createPayInfo(){
 function payOrder(payInfo){
     console.log("start wx pay",payInfo);
     wx.config({
-        debug:true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug:false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: payInfo.appId, // 必填，公众号的唯一标识
         timestamp:payInfo.timeStamp , // 必填，生成签名的时间戳
         nonceStr: payInfo.nonceStr, // 必填，生成签名的随机串
-        signature: payInfo.signature,// 必填，签名
+        signature: payInfo.paySign,// 必填，签名
         jsApiList: [
-           // 'onMenuShareTimeline', 'onMenuShareAppMessage','onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone',
-          //'updateAppMessageShareData',
-          //'updateTimelineShareData',
-          //'onMenuShareAppMessage',
-          //'onMenuShareTimeline',
-          'chooseWXPay',
-          //'showOptionMenu',
-          //"hideMenuItems",
-          //"showMenuItems",
-          //"onMenuShareTimeline",
-          //'onMenuShareAppMessage'                   
+          'chooseWXPay'                  
         ] // 必填，需要使用的JS接口列表
     });
     wx.ready(function() {
