edc6fd3fa658ba13651890eee2594c2f27dc9d38
Alex Chew <alex_chew@msn.com>
2022-04-12 08:13:44 +0800

    bug fix

--- publisher/js/my.js
+++ publisher/js/my.js
@@ -1049,8 +1049,7 @@ function createPayInfo(){
 
 //支付：发起微信支付提交购买。支付成功后创建购买记录
 function payOrder(payInfo){
-    var json = payInfo;
-    console.log("start wx pay",json);
+    console.log("start wx pay",payInfo);
     $.ajax({
         url:app.config.auth_api+"/wechat/jssdk/ticket",
         type:"get",
@@ -1058,7 +1057,7 @@ function payOrder(payInfo){
         success:function(json){
             console.log("===got jssdk ticket===\n",json);
             wx.config({
-                debug:false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug:true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: json.appId, // 必填，公众号的唯一标识
                 timestamp:json.timestamp , // 必填，生成签名的时间戳
                 nonceStr: json.nonceStr, // 必填，生成签名的随机串
@@ -1080,13 +1079,13 @@ function payOrder(payInfo){
             wx.ready(function() {
                 // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，
                 // 则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-              
+                console.log("before wx.chooseWXPay. payInfo.",payInfo);
                 wx.chooseWXPay({
                     appId: payInfo.appId,
-                  timeStamp: payInfo.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                  timestamp: payInfo.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                   nonceStr: payInfo.nonceStr, // 支付签名随机串，不长于 32 位
                   package: 'prepay_id='+payInfo.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-                  signType: 'RSA', // 微信支付V3的传入RSA,微信支付V2的传入格式与V2统一下单的签名格式保持一致
+                  signType: 'MD5', // 微信支付V3的传入RSA,微信支付V2的传入格式与V2统一下单的签名格式保持一致
                   paySign: payInfo.paySign, // 支付签名
                   success: function (res) {
                     // 支付成功后的回调函数
