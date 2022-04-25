14695f7f4d2d5de195d506f59752d824122e31aa
Alex Chew <alex_chew@msn.com>
2022-04-25 10:38:56 +0800

    enable inst subscribe

--- js/common.js
+++ js/common.js
@@ -39,6 +39,13 @@ function insertBroker(broker){
     $("#brokerHint").html("达人级别："+broker.level);
 }
 
+//生成UUID
+function getUUID() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
 //生成短码
 function generateShortCode(url){
     var chars = "0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ".split("");
